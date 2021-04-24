e2aafa7ea3b30ed9c909d3f6247d55b1fe0c025e
Ngireddy <somulanagireddy@gmail.com>
2021-04-24 14:56:23 +0530

    [Nagireddy]: added Exception for user provide invalid mood

--- src/main/java/com/bridgelabz/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/MoodAnalyzer.java
@@ -1,14 +1,23 @@
 package com.bridgelabz;
 
 public class MoodAnalyzer {
+    String message;
+    public MoodAnalyzer(String input) {
+        message = input;
+    }
 
-    public String analyseMood(String message) {
-        if (message.contains("Sad")) {
-            return "SAD";
-        } else {
+    public String analyseMood()  {
+        try {
+            if (message.contains("Sad")) {
+                return "SAD";
+            } else {
+                return "HAPPY";
+            }
+        } catch (NullPointerException e){
             return "HAPPY";
         }
     }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Mood Analyzer");
     }
--- src/test/java/com/bridgelabz/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyzerTest.java
@@ -6,16 +6,26 @@ import org.junit.jupiter.api.Test;
 
 
 public class MoodAnalyzerTest {
+
     @Test
     public void givenMessage_whenSad_ShouldReturnSad() {
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-        String mood = moodAnalyzer.analyseMood("This is a Sad Message");
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is a Sad Message");
+        String mood = moodAnalyzer.analyseMood();
         Assertions.assertEquals("SAD", mood);
     }
+
     @Test
     public void givenMessage_whenNotSad_ShouldReturnHappy()  {
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-        String mood = moodAnalyzer.analyseMood("This is a Happy Message");
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is a Happy Message");
+        String mood = moodAnalyzer.analyseMood();
         Assertions.assertEquals("HAPPY", mood);
     }
+
+    @Test
+    public void givenNullMood_ShouldReturnHappy() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
+        String mood = moodAnalyzer.analyseMood();
+        Assertions.assertEquals("HAPPY", mood);
+    }
+
 }
